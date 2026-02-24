51b7087c97076b3502a4f88e486673f7ea9c3755
Guerrero <andresbarrera5544@gmail.com>
2026-02-24 12:37:41 -0500

    tarea de python

--- python/data_types.py
+++ python/data_types.py
@@ -1,2 +1,13 @@
-print("Hello everyone. Welcome to data types")
+# This is my first Python Script
+'''
+   Developer: Andres Guerrero
+   Dscription: This script let us understand how to is data types.
+'''
+print("hello everyone. Welcome to data types class")
 
+age = 17
+print(type(age))
+EARTH_GRAVITY = 9.8
+PI = 3.1416
+print(type(EARTH_GRAVITY))
+print(type(PI))
---
+++ python/vaiables.py
@@ -0,0 +1,4 @@
+first_name = "Andres"
+print(type(first_name))
+print(first_name)
+print("your name is:", first_name)
